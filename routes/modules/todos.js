fccab60b25ef9c5a8f67e03b6512b6f82836c498
Jackson Hsieh <jackson97042@gmail.com>
2021-01-27 21:09:37 +0800

    feat: add edit feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,6 +3,28 @@ const router = express.Router()
 const db = require('../../models') //'./models/index.js'
 const Todo = db.Todo
 
+router.get('/:id/edit', (req, res) => {
+  const userId = req.user.id
+  const id = req.params.id  
+  return Todo.findOne({ where: { id, UserId: userId } })
+      .then(todo => res.render('edit', { todo: todo.toJSON() }))
+      .catch(error => console.error(error))
+})
+
+router.put('/:id', (req, res) => { 
+  const userId = req.user.id
+  const id = req.params.id  
+  const { name, isDone } = req.body
+  return Todo.findOne({ where: { id, UserId: userId } })   //need complete object (not plain)
+      .then(todo => {
+          todo.name = name
+          todo.isDone = isDone === 'on' //return true or false
+          return todo.save()
+      })
+      .then(() => res.redirect(`/todos/${id}`))
+      .catch(error => console.error(error))
+})
+
 router.get('/new', (req, res) => {
   return res.render('new')
 })

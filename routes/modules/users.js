f005dc7800657da6046dc3813835dbc6ebd389c0
Jackson Hsieh <jackson97042@gmail.com>
2021-01-27 19:49:31 +0800

    refactor: add router modules

---
+++ routes/modules/users.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+const db = require('../../models') //'./models/index.js'
+const User = db.User
+
+router.get('/login', (req, res) => {
+  console.log(req.flash('error'))
+  res.render('login')
+})
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login',
+  failureFlash: true
+}))
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
+})
+
+router.get('/logout', (req, res) => {
+  res.send('logout')
+})
+
+module.exports = router
